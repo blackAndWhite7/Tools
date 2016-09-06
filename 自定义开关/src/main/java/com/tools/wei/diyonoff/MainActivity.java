3bc5f0d7a2bbc356be9e66fb19b76ceb55bec002
blackAndWhite7 <651600056@qq.com>
2016-09-06 23:16:55 +0800

    下拉选择  图片轮播  自定义开关

---
+++ 自定义开关/src/main/java/com/tools/wei/diyonoff/MainActivity.java
@@ -0,0 +1,33 @@
+package com.tools.wei.diyonoff;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ToggleView toggleView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().hide();
+
+        toggleView = (ToggleView) findViewById(R.id.toggleView);
+        //        toggleView.setSwitchBackgroundResource(R.drawable.switch_background);
+        //        toggleView.setSlideButtonResource(R.drawable.slide_button);
+        //        toggleView.setSwitchState(true);
+        //
+        // 设置开关更新监听
+        toggleView.setOnSwitchStateUpdateListener(new ToggleView.OnSwitchStateUpdateListener() {
+
+            @Override
+            public void onStateUpdate(boolean state) {
+                Toast.makeText(getApplicationContext(), "state: " + state, 0).show();
+            }
+
+        });
+    }
+}
+
